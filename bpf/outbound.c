9983984fcd5f74d20314135b94bef39c3bc8afe6
Nicolas Said <nicolas.said@gmail.com>
2025-07-28 09:09:30 +0200

    chore: on my way to outbound support ?

--- bpf/outbound.c
+++ bpf/outbound.c
@@ -1,119 +1,99 @@
 #include <linux/types.h>
 #include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <linux/icmp.h>
-#include <linux/in.h> // Added for IPPROTO_*
-
+#include <linux/pkt_cls.h>
+#include <linux/in.h>
+#include <bpf/bpf_helpers.h>
 
 struct rule_key {
     __u32 src_ip;
     __u32 dst_ip;
     __u16 src_port;
     __u16 dst_port;
-    __u8 protocol;
-    __u8 icmp_type;
+    __u8  protocol;
     __u32 src_ip_mask;
     __u32 dst_ip_mask;
 };
 
 struct rule_value {
-    __u8 action; // 0: DROP, 1: ACCEPT
+    __u8 action;
 };
 
-struct bpf_map_def SEC("maps") outbound_rules = {
-    .type = BPF_MAP_TYPE_HASH,
-    .max_entries = 1000,
-    .key_size = sizeof(struct rule_key),
-    .value_size = sizeof(struct rule_value),
-};
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, struct rule_key);
+    __type(value, struct rule_value);
+    __uint(max_entries, 256);
+} outbound_rules SEC(".maps");
 
-struct bpf_map_def SEC("maps") outbound_default_policy = {
-    .type = BPF_MAP_TYPE_ARRAY,
-    .max_entries = 1,
-    .key_size = sizeof(__u32),
-    .value_size = sizeof(__u8),
-};
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, __u8);
+} outbound_default_policy SEC(".maps");
 
+// Use "tc" section for TC hooks
 SEC("tc")
-int tc_firewall_outbound(struct __sk_buff *ctx) {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
-    struct ethhdr *eth = data;
-    struct iphdr *ip;
-    struct tcphdr *tcp;
-    struct udphdr *udp;
-    struct icmphdr *icmp;
-    struct rule_key key = {};
-    struct rule_value *value;
-    __u32 default_key = 0;
-    __u8 *default_action;
+int tc_firewall_outbound(struct __sk_buff *skb) {
+    // void *data_end = (void *)(long)skb->data_end;
+    // void *data = (void *)(long)skb->data;
 
-    // Check Ethernet header
-    if (data + sizeof(*eth) > data_end)
-        return TC_ACT_OK;
+    // struct ethhdr *eth = data;
+    // if ((void *)eth + sizeof(*eth) > data_end)
+    //     return TC_ACT_OK;
 
-    if (eth->h_proto != __constant_htons(ETH_P_IP))
-        return TC_ACT_OK;
+    // if (eth->h_proto != __constant_htons(ETH_P_IP))
+    //     return TC_ACT_OK;
 
-    // Check IP header
-    ip = data + sizeof(*eth);
-    if ((void *)ip + sizeof(*ip) > data_end)
-        return TC_ACT_OK;
+    // struct iphdr *ip = data + sizeof(*eth);
+    // if ((void *)ip + sizeof(*ip) > data_end)
+    //     return TC_ACT_OK;
 
-    key.src_ip = ip->saddr;
-    key.dst_ip = ip->daddr;
-    key.protocol = ip->protocol;
-    key.src_ip_mask = 0xffffffff;
-    key.dst_ip_mask = 0xffffffff;
+    // __u32 src_ip = ip->saddr;
+    // __u32 dst_ip = ip->daddr;
+    // __u8 protocol = ip->protocol;
+    // __u16 src_port = 0, dst_port = 0;
 
-    // Handle protocols
-    if (ip->protocol == IPPROTO_TCP) {
-        tcp = (void *)ip + (ip->ihl * 4);
-        if ((void *)tcp + sizeof(*tcp) > data_end)
-            return TC_ACT_OK;
-        key.src_port = __constant_ntohs(tcp->source);
-        key.dst_port = __constant_ntohs(tcp->dest);
-    } else if (ip->protocol == IPPROTO_UDP) {
-        udp = (void *)ip + (ip->ihl * 4);
-        if ((void *)udp + sizeof(*udp) > data_end)
-            return TC_ACT_OK;
-        key.src_port = __constant_ntohs(udp->source);
-        key.dst_port = __constant_ntohs(udp->dest);
-    } else if (ip->protocol == IPPROTO_ICMP) {
-        icmp = (void *)ip + (ip->ihl * 4);
-        if ((void *)icmp + sizeof(*icmp) > data_end)
-            return TC_ACT_OK;
-        key.icmp_type = icmp->type;
-    }
+    // // Calculate IP header length in bytes
+    // __u32 ip_hdr_len = ip->ihl * 4;
+    // void *l4hdr = (void *)ip + ip_hdr_len;
 
-    // Check rules
-    value = bpf_map_lookup_elem(&outbound_rules, &key);
-    if (value) {
-        if (value->action == 1)
-            return TC_ACT_OK;
-        return TC_ACT_SHOT;
-    }
+    // // Bounds check before accessing TCP/UDP header
+    // if (protocol == IPPROTO_TCP) {
+    //     if (l4hdr + sizeof(struct tcphdr) > data_end)
+    //         return TC_ACT_OK;
+    //     struct tcphdr *tcp = l4hdr;
+    //     src_port = __constant_ntohs(tcp->source);
+    //     dst_port = __constant_ntohs(tcp->dest);
+    // } else if (protocol == IPPROTO_UDP) {
+    //     if (l4hdr + sizeof(struct udphdr) > data_end)
+    //         return TC_ACT_OK;
+    //     struct udphdr *udp = l4hdr;
+    //     src_port = __constant_ntohs(udp->source);
+    //     dst_port = __constant_ntohs(udp->dest);
+    // }
 
-    // Try with "any" IP (0.0.0.0/0)
-    key.src_ip = 0;
-    key.dst_ip = 0;
-    key.src_ip_mask = 0;
-    key.dst_ip_mask = 0;
-    value = bpf_map_lookup_elem(&outbound_rules, &key);
-    if (value) {
-        if (value->action == 1)
-            return TC_ACT_OK;
-        return TC_ACT_SHOT;
-    }
+    // struct rule_key lookup_key = {
+    //     .src_ip = src_ip,
+    //     .dst_ip = dst_ip,
+    //     .src_port = src_port,
+    //     .dst_port = dst_port,
+    //     .protocol = protocol,
+    //     .src_ip_mask = 0xffffffff,
+    //     .dst_ip_mask = 0xffffffff,
+    // };
 
-    // Apply default policy
-    default_action = bpf_map_lookup_elem(&outbound_default_policy, &default_key);
-    if (default_action && *default_action == 1)
-        return TC_ACT_OK;
+    // struct rule_value *value = bpf_map_lookup_elem(&outbound_rules, &lookup_key);
+    // if (value) {
+    //     return value->action ? TC_ACT_OK : TC_ACT_SHOT;
+    // }
+
+    // Default: accept
     return TC_ACT_SHOT;
 }
 
